5be1b54986f3308c0fdcae4a54e0e67a424139d7
yashithab <yashithab@gmail.com>
2021-07-07 15:20:28 -0400

    Day 12 completed

---
+++ Practice/Day_12.py
@@ -0,0 +1,15 @@
+################### Scope ####################
+
+enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+#Local Scope vs. global scope
+#Does Python have block scope:
+    # No such thing in python 
+
